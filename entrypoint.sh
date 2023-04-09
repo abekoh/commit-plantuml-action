f3cf4dea7debb6dd466d99692caa32b06621bbcf
Kotaro Abe <abekoh.bcky@gmail.com>
2023-04-09 16:07:25 +0900

    debug: log

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,6 +19,7 @@ git config --global --add safe.directory ${GITHUB_WORKSPACE}
 cd ${GITHUB_WORKSPACE}
 git fetch
 SRC_FILES=$(git diff origin/${GITHUB_BASE_REF} --name-only | grep ".puml")
+echo "src_files:"
 echo $SRC_FILES
 for SRC_FILE in ${SRC_FILES}; do
   java -jar /plantuml.jar $SRC_FILE -charset UTF-8
